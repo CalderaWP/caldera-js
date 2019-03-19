ae81aea8def7cc4f974edfa8235ff6e49aef9f50
Josh Pollock <josh@calderawp.com>
2019-03-19 16:37:39 -0400

    impliemnt all conditional rule types in createField Rule

--- packages/forms/src/CalderaForm/state/createFieldRule.js
+++ packages/forms/src/CalderaForm/state/createFieldRule.js
@@ -1,29 +1,87 @@
 /**
  * Create a conditional rule
  *
- * @param {string} test
+ * @param {string} testType
  * @param {string} fieldId
  * @param {string|int|array} testValue
  * @return {function(*=): boolean}
  */
-export const createFieldRule = (test,fieldId,testValue) => {
+export const createFieldRule = (testType,fieldId,testValue) => {
 	const findFieldValue= (fieldId,fieldValues) => {
 		return fieldValues.hasOwnProperty(fieldId)
 			?fieldValues[fieldId]
 			: null;
 	};
 
-	switch(test){
+	switch(testType){
 		case 'is':
+		case '==':
 			return (fieldValues) => {
 				const value = findFieldValue(fieldId,fieldValues);
 				return value == testValue;
 			};
+		case '====':
+			return (fieldValues) => {
+				const value = findFieldValue(fieldId,fieldValues);
+				return value === testValue;
+			};
+		case 'isnot':
 		case 'not':
+		case '!=':
+			return (fieldValues) => {
+				const value = findFieldValue(fieldId,fieldValues);
+				return value != testValue;
+			};
+		case '!==':
 			return (fieldValues) => {
 				const value = findFieldValue(fieldId,fieldValues);
 				return value !== testValue;
-			}
+			};
+		case '>':
+		case 'greater':
+			return (fieldValues) => {
+				const value = findFieldValue(fieldId,fieldValues);
+				return value > testValue;
+			};
+		case '<':
+		case 'smaller':
+			return (fieldValues) => {
+				const value = findFieldValue(fieldId,fieldValues);
+				return value < testValue;
+			};
+
+		case 'startswith':
+			return (fieldValues) => {
+				const value = findFieldValue(fieldId,fieldValues);
+				if( 'object' === typeof  value ){
+					return false;
+				}
+				return value.toLowerCase().substr(0, testValue.toLowerCase().length ) === testValue.toLowerCase();
+			};
+
+		case 'endswith':
+
+			return (fieldValues) => {
+				const value = findFieldValue(fieldId, fieldValues);
+				if ('object' === typeof  value) {
+					return false;
+				}
+
+				return value.toLowerCase().substr(testValue.toLowerCase().length - testValue.toLowerCase().length) === testValue.toLowerCase();
+			};
+		case 'contains':
+			return (fieldValues) => {
+				const value = findFieldValue(fieldId, fieldValues);
+				if ('object' === typeof  value) {
+					return false;
+				}
+
+				return values.toLowerCase().indexOf( testValue ) >= 0;
+
+			};
+
+		default:
+			return () => false;
 
 	};
 }
--- packages/forms/src/CalderaForm/state/createFieldRule.test.js
+++ packages/forms/src/CalderaForm/state/createFieldRule.test.js
@@ -1,7 +1,7 @@
 import {createFieldRule} from './createFieldRule';
 describe('createFieldRule', () => {
 	it('Creates function for is conditional', () => {
-		expect( typeof createFieldRule('is', 'd', {}));
+		expect( typeof createFieldRule('is', 'd', {})).toBe( 'function');
 	});
 
 	it('Is conditional is true when it should be', () => {
@@ -60,4 +60,105 @@ describe('createFieldRule', () => {
 		const rule = createFieldRule('not', 'f1', 'pa' );
 		expect( rule(fieldValues)).toBe(false);
 	});
+
+	it( 'compares with weak equality', () => {
+		const testValue = '22';
+		const fieldValues = {
+			f2: 22,
+			f44: 'Hi'
+		};
+		let rule = createFieldRule('is', 'f2', '22');
+		expect( rule(fieldValues)).toBe(true);
+		let rule = createFieldRule('is', 'f2', 22 );
+		expect( rule(fieldValues)).toBe(true);
+		rule = createFieldRule('==', 'f2', '22');
+		expect( rule(fieldValues)).toBe(true);
+
+	});
+
+	it( 'compares with strong equality', () => {
+		const testValue = '22';
+		const fieldValues = {
+			f2: 22,
+			f44: 'hats'
+		};
+		let rule = createFieldRule('===', 'f2', '22' );
+		expect( rule(fieldValues)).toBe(false);
+		rule = createFieldRule('===', 'f2', 22);
+		expect( rule(fieldValues)).toBe(true);
+		rule = createFieldRule('===', 'f44', 'hats');
+		expect( rule(fieldValues)).toBe(true);
+		rule = createFieldRule('===', 'f44', 'hatball' );
+		expect( rule(fieldValues)).toBe(false);
+	});
+
+	it( 'compares greater than', () => {
+		const testValue = '22';
+		const fieldValues = {
+			f2: 22,
+			f44: '22'
+		};
+		let rule = createFieldRule('>', 'f2', 20);
+		expect( rule(fieldValues)).toBe(true);
+		rule = createFieldRule('>', 'ft', 11000 );
+		expect( rule(fieldValues)).toBe(false);
+		rule = createFieldRule('>', 'f2', '20');
+		expect( rule(fieldValues)).toBe(true);
+		rule = createFieldRule('>', 'f2', '11000' );
+		expect( rule(fieldValues)).toBe(false);
+
+	});
+
+	it( 'compares less than', () => {
+		const testValue = '22';
+		const fieldValues = {
+			f2: 22,
+			f44: '22'
+		};
+		let rule = createFieldRule('<', 'f2', 20);
+		expect( rule(fieldValues)).toBe(false);
+		rule = createFieldRule('<', 'f2', 11000 );
+		expect( rule(fieldValues)).toBe(true);
+		rule = createFieldRule('<', 'f2', '20');
+		expect( rule(fieldValues)).toBe(false);
+		rule = createFieldRule('<', 'f2', '11000' );
+		expect( rule(fieldValues)).toBe(true);
+
+	});
+
+	it( 'compares less than with >', () => {
+		const testValue = '22';
+		const fieldValues = {
+			f2: 22,
+			f44: '22'
+		};
+		let rule = createFieldRule('>', 'f2', 20);
+		expect( rule(fieldValues)).toBe(true);
+		rule = createFieldRule('>', 'f2', 11000 );
+		expect( rule(fieldValues)).toBe(false);
+		rule = createFieldRule('>', 'f2', '20');
+		expect( rule(fieldValues)).toBe(true);
+		rule = createFieldRule('>', 'f2', '11000' );
+		expect( rule(fieldValues)).toBe(false);
+
+	});
+
+		it( 'compares less than with smaller', () => {
+		const testValue = '22';
+		const fieldValues = {
+			f2: 22,
+			f44: '22'
+		};
+		let rule = createFieldRule('smaller', 'f2', 20);
+		expect( rule(fieldValues)).toBe(true);
+		rule = createFieldRule('smaller', 'f2', 11000 );
+		expect( rule(fieldValues)).toBe(false);
+		rule = createFieldRule('smaller', 'f2', '20');
+		expect( rule(fieldValues)).toBe(true);
+		rule = createFieldRule('smaller', 'f2', '11000' );
+		expect( rule(fieldValues)).toBe(false);
+
+	});
+
+
 });
