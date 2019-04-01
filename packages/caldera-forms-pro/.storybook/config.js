209f03bd15813da7c9bff86c42c9b6142aa02850
Josh Pollock <josh@calderawp.com>
2019-04-01 14:24:18 -0400

    delete cfpro web app prototype

--- packages/caldera-forms-pro/.storybook/config.js
+++
@@ -1,9 +0,0 @@
-import { configure } from '@storybook/react'
-
-const storiesDirStories = require.context('../stories', true, /.stories.js$/)
-const srcDirStories = require.context('../src', true, /.stories.js$/)
-function loadStories () {
-	storiesDirStories.keys().forEach(filename => storiesDirStories(filename))
-}
-
-configure(loadStories, module)
