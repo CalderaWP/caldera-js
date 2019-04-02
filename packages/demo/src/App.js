ecc60d6342cf3dbccdca55f796fefc42add200bf
Josh Pollock <josh@calderawp.com>
2019-04-02 14:36:35 -0400

    move formConfig out of component

--- packages/demo/src/App.js
+++ packages/demo/src/App.js
@@ -6,41 +6,6 @@ import {
 } from "@calderajs/forms";
 import axios from 'axios';
 
-const firstNameField = {
-	fieldType: 'text',
-	value: '',
-	label: 'First Name',
-	fieldId: 'fld_8768091',
-	description: 'Your first name',
-	required: true
-};
-
-const lastNameField = {
-	fieldType: 'text',
-	value: '',
-	label: 'Last Name',
-	fieldId: 'fld_9970286',
-	description: 'Your last name',
-	required: true
-};
-const emailField = {
-	fieldType: 'email',
-	value: '',
-	label: 'Email',
-	fieldId: 'fld_6009157',
-	description: 'Your email',
-	required: true,
-	attributes: {
-		multiple: false
-	}
-};
-
-const submitButton = {
-	fieldId: 'fld_7908577',
-	label: 'Click Me',
-	fieldType: 'submit',
-};
-
 
 async function submitForm(
 	{
@@ -70,7 +35,8 @@ async function submitForm(
 	}
 }
 
-const App = ({apiRootUri, formId}) => {
+
+const App = ({apiRootUri, formId,formConfig}) => {
 
 	const [formLoaded, setFormLoaded] = useState(false);
 	const [message, updateMessage] = useState('Effect has not run yet');
@@ -79,52 +45,7 @@ const App = ({apiRootUri, formId}) => {
 		_cf_verify: '',
 		_sessionPublicKey: ''
 	});
-	const [form, setForm] = useState({
-		rows: [
-			{
-				rowId: 'r1',
-				columns: [
-					{
-						fields: [firstNameField.fieldId],
-						width: '1/2',
-						columnId: `r1-${firstNameField.fieldId}`
-					},
-					{
-						fields: [lastNameField.fieldId],
-						width: '1/4',
-						columnId: `r1-${lastNameField.fieldId}`
-					}
-				]
-			},
-			{
-				rowId: 'r2',
-				columns: [
-					{
-						fields: [emailField.fieldId],
-						width: '1',
-						columnId: `r2-${emailField.fieldId}`
-					},
-				]
-			},
-			{
-				rowId: 'r3',
-				columns: [
-					{
-						fields: [submitButton.fieldId],
-						width: '1',
-						columnId: `r2-${emailField.fieldId}`
-					},
-				]
-			}
-		],
-		fields: [
-			emailField,
-			firstNameField,
-			lastNameField,
-			submitButton
-		],
-		conditionals: []
-	});
+	const [form, setForm] = useState(formConfig);
 
 
 	useEffect(() => {
--- packages/demo/src/index.js
+++ packages/demo/src/index.js
@@ -4,9 +4,92 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
+const firstNameField = {
+	fieldType: 'text',
+	value: '',
+	label: 'First Name',
+	fieldId: 'fld_8768091',
+	description: 'Your first name',
+	required: true
+};
+
+const lastNameField = {
+	fieldType: 'text',
+	value: '',
+	label: 'Last Name',
+	fieldId: 'fld_9970286',
+	description: 'Your last name',
+	required: true
+};
+const emailField = {
+	fieldType: 'email',
+	value: '',
+	label: 'Email',
+	fieldId: 'fld_6009157',
+	description: 'Your email',
+	required: true,
+	attributes: {
+		multiple: false
+	}
+};
+
+const submitButton = {
+	fieldId: 'fld_7908577',
+	label: 'Click Me',
+	fieldType: 'submit',
+};
+
+const formConfig = {
+	rows: [
+		{
+			rowId: 'r1',
+			columns: [
+				{
+					fields: [firstNameField.fieldId],
+					width: '1/2',
+					columnId: `r1-${firstNameField.fieldId}`
+				},
+				{
+					fields: [lastNameField.fieldId],
+					width: '1/4',
+					columnId: `r1-${lastNameField.fieldId}`
+				}
+			]
+		},
+		{
+			rowId: 'r2',
+			columns: [
+				{
+					fields: [emailField.fieldId],
+					width: '1',
+					columnId: `r2-${emailField.fieldId}`
+				},
+			]
+		},
+		{
+			rowId: 'r3',
+			columns: [
+				{
+					fields: [submitButton.fieldId],
+					width: '1',
+					columnId: `r2-${emailField.fieldId}`
+				},
+			]
+		}
+	],
+	fields: [
+		emailField,
+		firstNameField,
+		lastNameField,
+		submitButton
+	],
+	conditionals: []
+};
+
 ReactDOM.render(<App
 	apiRootUri={'http://dev-futurecapable.pantheonsite.io/wp-json/cf-api'}
 	formId={'CF5c9f86904e447'}
+	formConfig={formConfig}
 />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
