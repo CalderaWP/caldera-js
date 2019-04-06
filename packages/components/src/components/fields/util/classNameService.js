95114355d3085627deec8657d42cf1ee0327b109
Josh Pollock <josh@calderawp.com>
2019-04-06 13:40:00 -0400

    failing unit tests #4

--- packages/components/src/components/fields/util/classNameService.js
+++ packages/components/src/components/fields/util/classNameService.js
@@ -3,4 +3,9 @@ let classNameHooks = createHooks();
 
 export const CLASS_NAME_HOOKS = 'classNameHooks';
 
+export const FILTER_FIELD_WRAPPER_CLASS_NAME = 'classNameHooks.fieldWrapperClassName';
+export const FILTER_FIELD_CLASS_NAME = 'classNameHooks.fieldClassName';
+export const FILTER_FIELD_LABEL_CLASS_NAME = 'classNameHooks.fieldLabelClassName';
+export const FILTER_FIELD_SET_CLASS_NAME = 'classNameHooks.fieldSetClassName';
+
 export default classNameHooks;
--- packages/components/src/components/fields/util/classNameService.test.js
+++ packages/components/src/components/fields/util/classNameService.test.js
@@ -1,14 +1,54 @@
-import classNameService,{CLASS_NAME_HOOKS} from './classNameService'
-import { createHooks } from '@wordpress/hooks';
-let globalHooks = createHooks();
+import classNameService, {
+	CLASS_NAME_HOOKS,
+	FILTER_FIELD_CLASS_NAME,
+	FILTER_FIELD_LABEL_CLASS_NAME,
+	FILTER_FIELD_SET_CLASS_NAME,
+	FILTER_FIELD_WRAPPER_CLASS_NAME
+} from './classNameService'
+import {fieldClassNames} from "./fieldClassNames";
 
-
-
-
-describe( 'classNameHooks', ()=> {
-	it( 'filters work as expected',()=> {
-		classNameService.addFilter('headerPath', CLASS_NAME_HOOKS, () => 'ALT!' );
-		const headerPath = classNameService.applyFilters('headerPath', './Header' );
-		expect( headerPath  ).toEqual( 'ALT!' )
+const TEST_CONSUMER = 'namespace';
+describe('classNameHooks', () => {
+	beforeEach(() => {
+		[FILTER_FIELD_CLASS_NAME,
+			FILTER_FIELD_LABEL_CLASS_NAME,
+			FILTER_FIELD_SET_CLASS_NAME,
+			FILTER_FIELD_WRAPPER_CLASS_NAME
+		].forEach(filterName => classNameService.removeAllFilters(filterName)
+		)
 	})
+	it('filters work as expected', () => {
+		classNameService.addFilter('headerPath', TEST_CONSUMER, () => 'ALT!');
+		const headerPath = classNameService.applyFilters('headerPath', './Header');
+		expect(headerPath).toEqual('ALT!')
+	});
+
+	const cf1ClassNames = {
+		FILTER_FIELD_CLASS_NAME: 'field-set',
+		FILTER_FIELD_SET_CLASS_NAME: 'form-control',
+		FILTER_FIELD_WRAPPER_CLASS_NAME: 'form-group',
+		FILTER_FIELD_LABEL_CLASS_NAME: 'control-label'
+	};
+
+
+
+	it('Adds className for field', () => {
+		classNameService.addFilter(FILTER_FIELD_CLASS_NAME, TEST_CONSUMER, () => cf1ClassNames[FILTER_FIELD_WRAPPER_CLASS_NAME]);
+		expect(fieldClassNames('text').includes(cf1ClassNames[FILTER_FIELD_WRAPPER_CLASS_NAME])).toBe(true);
+	});
+
+	it('Adds className for fieldset', () => {
+		classNameService.addFilter(FILTER_FIELD_SET_CLASS_NAME, TEST_CONSUMER, () => cf1ClassNames[FILTER_FIELD_SET_CLASS_NAME]);
+		expect(fieldClassNames('text').includes(cf1ClassNames[FILTER_FIELD_SET_CLASS_NAME])).toBe(true);
+	});
+
+	it('Adds className for field wrapper', () => {
+		classNameService.addFilter(FILTER_FIELD_WRAPPER_CLASS_NAME, TEST_CONSUMER, cf1ClassNames[FILTER_FIELD_WRAPPER_CLASS_NAME]);
+		expect(fieldClassNames('text').includes('form-group')).toBe(true);
+	});
+
+	it('Adds className for field label', () => {
+		classNameService.addFilter(FILTER_FIELD_LABEL_CLASS_NAME, TEST_CONSUMER, () => cf1ClassNames[FILTER_FIELD_LABEL_CLASS_NAMEg]);
+		expect(fieldClassNames('text')).includes('form-group').toBe(true);
+	});
 });
