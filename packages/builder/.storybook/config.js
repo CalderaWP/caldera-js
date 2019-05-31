cce4c3a97f2f9fc785d5aaf75a05f9ad5df836d7
Josh Pollock <josh@calderawp.com>
2019-05-31 15:14:20 -0400

    add addons to storybook for builder that do not work

--- packages/builder/.storybook/config.js
+++ packages/builder/.storybook/config.js
@@ -1,7 +1,10 @@
 //https://github.com/storybooks/storybook/issues/489#issuecomment-297426989
 import 'babel-polyfill';
-import { configure } from '@storybook/react';
+import { configure,addDecorator } from '@storybook/react';
+import { withA11y } from '@storybook/addon-a11y';
 
+
+addDecorator(withA11y)
 function loadStories() {
     require('glob-loader!./stories.pattern')
 }
